82849490e8860d6832617c781212b4d67d82c090
naveen <navi7812931281@gmail.com>
2026-03-30 12:23:35 +0530

    Added routing and controller layer

--- src/routers/ping.router.ts
+++ src/routers/ping.router.ts
@@ -3,17 +3,6 @@ import { pingHandler } from '../controllers/ping.controller.ts';
 
 const pingRouter =express.Router();
 
-function middleware1(req: Request , res:Response,next:NextFunction){
-    console.log('Middleware 1');
-    next();
-}
-
-function middleware2(req: Request , res:Response,next:NextFunction){
-    console.log('Middleware 2');
-    next();
-}
-
-
 pingRouter.get('/ping',middleware1,middleware2,pingHandler);
 
 export default pingRouter;
